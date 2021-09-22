80b011a949a6855091e362acc466a77f3025e69b
jheinath <julianheinath@gmail.com>
2021-09-22 11:10:43 +0200

    added interface repo, added first command

---
+++ Adapters.Database/Repositories/ConfigurationRepository/IConfigurationRepository.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Aggregates.Configuration;
+using System.Threading.Tasks;
+
+namespace Adapters.Database.Repositories.ConfigurationRepository
+{
+    public interface IConfigurationRepository
+    {
+        Task InsertAsync(Configuration configuration);
+    }
+}
